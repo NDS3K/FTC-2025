165b53fcd63d5ff7d12800775ed14bbbde3e9383
NDS3K <160611407+NDS3K@users.noreply.github.com>
2025-02-14 13:07:29 +0700

    Update Scoring.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/Scoring.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/Scoring.java
@@ -2,48 +2,88 @@ package org.firstinspires.ftc.teamcode.mechanism;
 
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.algorithms.PID;
 
-
 public class Scoring {
-    public DcMotorEx linear, elbow;
+    public DcMotorEx linear;
+    public Servo elbow;
+    public Servo claw;
+    public Servo wrist;
     PID pid = new PID();
     public Scoring(HardwareMap hardwareMap){
         linear = hardwareMap.get(DcMotorEx.class, "ln");
-        elbow = hardwareMap.get(DcMotorEx.class, "elb");
         linear.setDirection(DcMotorEx.Direction.REVERSE);
-        elbow.setDirection(DcMotorEx.Direction.REVERSE);
-        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linear.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        elbow = hardwareMap.get(Servo.class, "el");
+        elbow.setPosition(0.64);
+        wrist = hardwareMap.get(Servo.class, "ws");
+        wrist.setPosition(0);
+        claw = hardwareMap.get(Servo.class, "cl");
+        claw.setPosition(1);
+
     }
-    public Action linearControl(boolean x, boolean y,double power){
-        return telemetryPacket ->  {
-            if(x) elbow.setPower(power);
-            else if(y) elbow.setPower(-power);
-            else elbow.setPower(0);
-            return false;
-        };
-    }
-    public Action elbowControl(double power) {
+    public Action elbowAuto(double position){
         return telemetryPacket -> {
-            linear.setPower(power);
+            elbow.setPosition(position);
             return false;
         };
     }
-    public Action scoringControl(boolean x, boolean y,double elbow_power, double linear_power){
-        return telemetryPacket ->{
-            Actions.runBlocking(new ParallelAction(
-                    elbowControl(elbow_power),
-                    linearControl(x,y,linear_power)
-            ));
+    public Action wsAuto(double position){
+        return telemetryPacket -> {
+            wrist.setPosition(position);
+            return false;
+        };
+    }
+    public Action clawAuto(double position){
+        return telemetryPacket -> {
+            claw.setPosition(position);
+            return false;
+        };
+    }
+    public Action linearAuto(double target, double power) {
+        return telemetryPacket -> {
+            double output = pid.PIDControlDistance(target, linear.getCurrentPosition());
+            linear.setPower(output * power);
             return false;
         };
     }
 
+    public Action linearControl(boolean x, boolean y,double power){
+        return telemetryPacket ->  {
+            if(x) linear.setPower(power);
+            else if(y) linear.setPower(-power);
+            else linear.setPower(0);
+            return false;
+        };
+    }
+    /*
+    public Action elbowControl(double power) {
+        return telemetryPacket -> {
+            elbow.setPower(power);
+            return false;
+        };
+    }
+    */
+    public Action scoringControl(boolean x, boolean y, boolean a, boolean b, boolean c, boolean d){
+        return telemetryPacket ->{
+            if(x) Actions.runBlocking(elbowAuto(.2));
+            else if(y) Actions.runBlocking(elbowAuto(0.8));
+            if(a) Actions.runBlocking(clawAuto(0.3));
+            else if(b) Actions.runBlocking(clawAuto(1));
+            if(c) Actions.runBlocking(wsAuto(1));
+            else if(d) Actions.runBlocking(wsAuto(0));
+            return false;
+
+        };
+    }
+
+    /*
     public Action elbowAuto(double target, double power){
         return telemetryPacket -> {
             double output = pid.PIDControlDistance(target, elbow.getCurrentPosition());
@@ -51,11 +91,16 @@ public class Scoring {
             return false;
         };
     }
+    */
 
-    public Action linearAuto(double target, double power) {
+    public Action scoringAutomation(double linear_target,double linear_power, int elbow_position, double wrist_position){
         return telemetryPacket -> {
-            double output = pid.PIDControlDistance(target, linear.getCurrentPosition());
-            linear.setPower(output * power);
+            Actions.runBlocking(new SequentialAction(
+                    linearAuto(linear_target,linear_power),
+                    elbowAuto(elbow_position),
+                    wsAuto(wrist_position),
+                    clawAuto(0.3)
+            ));
             return false;
         };
     }
