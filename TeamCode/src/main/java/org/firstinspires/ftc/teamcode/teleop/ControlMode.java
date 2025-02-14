a17085c2c903d69b4e9c3deb742835973b130484
NDS3K <160611407+NDS3K@users.noreply.github.com>
2025-02-14 13:08:09 +0700

    Update ControlMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ControlMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ControlMode.java
@@ -5,6 +5,7 @@ import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.ColorSen;
 import org.firstinspires.ftc.teamcode.algorithms.PID;
 import org.firstinspires.ftc.teamcode.mechanism.Drivetrain;
 import org.firstinspires.ftc.teamcode.mechanism.Intake;
@@ -17,14 +18,15 @@ public class ControlMode extends LinearOpMode {
         Intake intake = new Intake(hardwareMap);
         Scoring scoring = new Scoring(hardwareMap);
         PID pid = new PID();
+        ColorSen colorSen = new ColorSen(hardwareMap);
         waitForStart();
         while(opModeIsActive()) {
+
             Actions.runBlocking(new ParallelAction(
-                    drivetrain.drive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x*0.7),
-                    intake.intakeControl(gamepad1.x,gamepad2.x,gamepad2.y, gamepad1.dpad_up, gamepad1.dpad_down, 0.8),
-                    scoring.scoringControl(gamepad1.a, gamepad1.b, gamepad1.right_stick_y*0.7, 1))
-            );
-            telemetry.addData("elbow:", scoring.elbow.getCurrentPosition());
+                    drivetrain.drive(gamepad1.left_stick_x*.65, -gamepad1.left_stick_y*.65, gamepad1.right_stick_x*0.7),
+                    intake.intakeControl(gamepad1.dpad_up, gamepad1.dpad_down, gamepad1.left_bumper, gamepad1.right_bumper,gamepad1.triangle, gamepad1.cross),
+                    scoring.scoringControl(gamepad1.circle, gamepad1.square, gamepad2.x, gamepad2.y, gamepad1.left_trigger>0, gamepad1.right_trigger>0)
+            ));
             telemetry.addData("linear:", scoring.linear.getCurrentPosition());
             telemetry.update();
         }
